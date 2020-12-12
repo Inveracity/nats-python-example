d6ef7767324ea7c82b17afe127a564b190c9733c
Christopher Baklid <christopherb@unity3d.com>
2020-12-12 13:53:17 +0100

    update generate task script

--- kronk/generate_tasks.py
+++ kronk/generate_tasks.py
@@ -1,23 +1,28 @@
-import time
+import logging
 from random import sample
 from string import ascii_letters
+from time import sleep
 from uuid import uuid4
 
-from database import Rethink
-from task import Task
+from kronk.database import Rethink
+from kronk.task import State
+from kronk.task import Task
 
-rdb  = Rethink()
-task = Task()
+log = logging.getLogger(__name__)
+
+rdb = Rethink()
 
 for _ in range(100):
-    random_chars  = sample(ascii_letters, 10)
+    task = Task()
+    random_chars = sample(ascii_letters, 10)
     random_string = "".join(random_chars)
     task.workload = random_string
-    task.state    = "ready"
-    task.id       = str(uuid4())
+    task.state = State.READY
+    task.id = str(uuid4())
+    task.time_updated = task.time()
 
-    print(f"Generating task: {random_string}")
+    log.info(f"Generating task: {random_string}")
 
-    rdb.task_create(task.to_dict())
+    rdb.task_create(task)
 
-    time.sleep(0.2)
+    sleep(0.2)
