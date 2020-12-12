79c00beee4b249351fc787280d623fa4125df9ad
Christopher Baklid <christopherb@unity3d.com>
2020-12-12 13:50:31 +0100

    make config a dataclass

--- kronk/config.py
+++ kronk/config.py
@@ -1,7 +1,18 @@
+from dataclasses import dataclass
 from os import environ
 
-config = {
-    "rdb_endpoint": environ.get("RDB_ENDPOINT", "127.0.0.1"),
-    "rdb_password": environ.get("RDB_PASSWORD", None),
-    "nats_endpoint": environ.get("NATS_ENDPOINT", "nats://127.0.0.1:4222/"),
-}
+
+@dataclass
+class Config:
+    DB_HOST: str = environ.get("DB_HOST", "127.0.0.1")
+    DB_PORT: str = environ.get("DB_PORT", "28015")
+    DB_PASS: str = environ.get("DB_PASSWORD", None)
+    NATS_HOST: str = environ.get("NATS_HOST", "127.0.0.1")
+    NATS_PORT: str = environ.get("NATS_HOST", "4222")
+
+    @property
+    def NATS_ENDPOINT(self) -> str:
+        """
+        combines protocol, host and port
+        """
+        return f"nats://{self.NATS_HOST}:{self.NATS_PORT}/"
